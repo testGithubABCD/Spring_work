a525bc8c6721312f668367e0b04ba0bcc4c3d582
test aa <1988makoto0901@gmail.com>
2020-02-19 00:19:13 +0900

    add database access

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,5 +1,10 @@
 package com.example.demo.trySpring;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+	
 	@GetMapping("/hello")
 	public String getHello() {
 		return "hello";
@@ -16,7 +24,11 @@ public class HelloController {
 
 	@PostMapping("/hello")
 	public String postRequest(@RequestParam("text1")String str, Model model) {
-		model.addAttribute("sample", str);
+		
+       List<Map<String,Object>> list;
+       list = jdbcTemplate.queryForList("select name from users where id = ?", 1);
+		
+		model.addAttribute("sample", list.toString());
 		return "helloResponse";
 	}
 	
